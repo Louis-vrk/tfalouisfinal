ed3c02f0cd57640f4960ce64767532a6aa435933
Louis Vereecke <louisvereecke123@outlook.fr>
2022-06-04 17:08:04 +0200

    ajout javascript darkmode

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
-console.info('Hello world');
+const darkTheme = document.querySelector(".darkmode__navbar");
+
+//data-theme body
+darkTheme.addEventListener("click", function(){
+    if(document.body.getAttribute("data-theme") === "dark"){
+        light();
+        localStorage.setItem("theme", "light");
+    } else {
+        dark();
+        localStorage.setItem("theme", "dark");
+    } 
+});
+
+//Does the user want a dark theme?
+const userDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+//Did the user already indicate a preference on our site?
+let theme = localStorage.getItem("theme");
+if((!theme && userDark) || (theme === "dark")){
+    dark();
+} else if(theme === "light"){
+    light();
+}
+
+//function dark
+function dark(){
+    document.body.setAttribute("data-theme", "dark");
+}
+//function light
+function light(){
+    document.body.setAttribute("data-theme", "light");
+}
